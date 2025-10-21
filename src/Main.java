3b8b4c7d8b082f7a04d37150425ba0f77ce71588
Laura-702 <Villaraza.Laura@student.greenriver.edu>
2025-10-20 19:40:55 -0700

    Answered fourth question

--- src/Main.java
+++ src/Main.java
@@ -41,7 +41,7 @@ public class Main {
 
 
   // The time complexity is:
-  // YOUR ANSWER HERE
+  // O(n) where n equals the length of nums 
   public static int computeProduct(int[] nums) {
     int total = 1;
     for(int num : nums) {
